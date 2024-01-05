9c8a0173589a4187c92225bb38c0165e3ccf9e09
Shawn X. Wang <xwang@fullerton.edu>
2024-01-04 22:45:21 -0800

    Add files via upload

---
+++ index.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<HTML><HEAD><TITLE>Dr. Shawn Wang in CSUF</TITLE>
+<meta content="text/html; charset=utf-8" http-equiv="Content-Type" />
+
+<link rel="stylesheet" type="text/css" href="./sxw.css">
+
+</HEAD>
+<BODY style="background-color: #EEEEEE; color: #000000">
+
+<div class="content-pad">
+<div class="style17">
+
+<a href="#main"><h3>Dr. Shawn X. Wang</h3></a>
+
+<div style="border-bottom-style: solid; border-bottom-width: 3px">
+<ul class="navbar">
+    <li><a href="./index.php">Home</a></li>
+    <li><a href="./schedule.php">Schedule</a></li>
+    <li><a href="./student.php">Students</a></li>
+    <li><a href="./pub.php">Publications</a></li>
+    <li><a href="./honor.php">Services &amp; Honors</a></li>
+    <li><a href="./teach.php">Experience</a></li>
+</ul>
+</div>
+
+<main id="main" role="main">
+
+<TABLE summary="formatting coverpage" border=0>
+  <TBODY>
+  <TR>
+    <TD style="padding: 5px 5px"><IMG align=left height=234 src="./images/Shawn X Wang.jpg" hspace="10px"
+      width=160 alt="Dr. Shawn Wang">
+ 
+ <P align=justify>
+<div class="style27">
+      Dr. Shawn X. Wang is a professor of computer science at Cal State Fullerton.
+      Dr. Wang holds a BS in mathematics from
+      <A href="http://www.xmu.edu.cn/">Xiamen University</A>,
+      an MS in computer science from
+      <A href="http://www.fudan.edu.cn/">Fudan University</A>,
+      and a PhD in computer and information science from
+      <A href="http://www.njit.edu/">New Jersey Institute of Technology</A>.
+      His research interests include data
+      mining, big data, cloud computing, bioinformatics, and cybersecurity.
+      Dr. Wang is a member of ACM, ACM SIGMOD.
+      He is listed in Who's Who in Science and Engineering and
+      Who's Who in America.
+</div>
+      </p>
+<br />
+<p>
+    <a href="./asmember.php">Wang was an Academic Senate member</a><br />
+    <a href="./titan.php">Wang's profile
+      in The Daily Titan</a><br />
+    <a href="./BioChinese.php">
+      Wang's profile in Chinese</a><br />
+<a href="./WAIM02.php">
+      Wang was a Publicity Chair and Panelist at WAIM'02</a><br />
+      </P></TD>
+  </TR></TBODY></TABLE>
+<HR align=center />
+<center>
+<TABLE summary="formatting address" border=0 width=900>
+  <TBODY>
+  <TR>
+    <TD align=left>
+<font color=black>
+      <A href="http://www.fullerton.edu/"><IMG align=right border=0 
+      src="./images/csuf.png" alt="Cal State Fullerton"></A>
+      <p><ADDRESS><B>Address:</B> Department of Computer Science<BR>
+      California State University, Fullerton<BR>
+      P.O. Box 6870, Fullerton, CA 92834-6870<BR>
+      <B>Office:</B> CS 532<BR><B>Office 
+      Hours:</B> Tue, Wed, and Thu 1:15 p.m. - 2:15 p.m. in office or via Zoom
+      <BR><B>Tel:</B>(657)278-7258 <B>Fax:</B>(657)278-7168 
+      <BR><B>E-mail:</B> xwang AT fullerton DOT edu
+  </ADDRESS></font>
+</p>
+<p><font size=-1><i>&copy; Dr. Shawn X Wang,
+Last update Feb 13, 2023</i></font></p></TD></TR></TBODY></TABLE>
+</center>
+
+</main>
+
+</div>
+</div>
+
+</BODY></HTML>
